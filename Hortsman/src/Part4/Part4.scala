4fdc9827949f3c79d0fca565355724bf8255d814
dimaLytvyniuk <dima20-12@outlook.com>
2018-07-20 07:26:59 +0300

    End part4 Hortsmann

--- Hortsman/src/Part4/Part4.scala
+++ Hortsman/src/Part4/Part4.scala
@@ -98,6 +98,68 @@ object Part4 extends App {
     println()
   }
 
+  def seventhTask(): Unit = {
+    val properties = Map(
+      "java.runtime.name" -> System.getProperty("java.runtime.name"),
+      "sun.boot.library.path" -> System.getProperty("sun.boot.library.path"),
+      "java.vm.version" -> System.getProperty("java.vm.version"),
+      "java.vm.vendor" -> System.getProperty("java.vm.vendor"),
+      "java.vendor.url" -> System.getProperty("java.vendor.url"),
+      "path.separator" -> System.getProperty("path.separator"),
+      "java.vm.name" -> System.getProperty("java.vm.name")
+    )
+
+    val max = properties.foldLeft(0)((max, kv) => if (max < kv._1.length) kv._1.length else max)
+
+    for ((k, v) <- properties)
+      println(String.format(s"%-${max + 10}s | %s", k, v))
+    println()
+  }
+
+  def minmax(values: Array[Int]): Tuple2[Int, Int] = {
+    values
+      .tail
+      .foldLeft((values(0), values(0)))((t, x) =>
+        if (t._1 > x)
+          (x, t._2)
+        else if (t._2 < x)
+          (t._1, x)
+        else
+          t)
+  }
+
+  def eighthTask(): Unit = {
+    val values = Array(1, 3, -8, 102, 3, 12, 3, -40)
+
+    val result = minmax(values)
+
+    println(s"Min: ${result._1}, Max: ${result._2}")
+  }
+
+  def lteqgt(values: Array[Int], v: Int): Tuple3[Int, Int, Int] = {
+    values.foldLeft((0, 0, 0))((t, x) =>
+      if(x < v)
+        (t._1 + 1, t._2, t._3)
+      else if (x > v)
+        (t._1, t._2, t._3 + 1)
+      else
+        (t._1, t._2 + 1, t._3))
+  }
+
+  def ninthTask(): Unit = {
+    val values = Array(1, 3, -8, 102, 3, 12, 3, -40)
+
+    val result = lteqgt(values, 3)
+
+    println(s"V = 3 Less: ${result._1}, Equal: ${result._2} Great: ${result._3}")
+  }
+
+  def tenthTask(): Unit = {
+    val zipResult = "Hello".zip("World");
+
+    for ((x, y) <- zipResult) print(s"$x$y ")
+  }
+
   firstTask()
 
   secondTask()
@@ -109,5 +171,13 @@ object Part4 extends App {
   fifthTask()
 
   sixthTask()
+
+  seventhTask()
+
+  eighthTask()
+
+  ninthTask()
+
+  tenthTask()
 }
 
